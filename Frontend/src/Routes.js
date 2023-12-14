6e3777cc03ac8184987920df5750c56454fa53ef
Osele Chidera <oselechideraemmanuel123@gmail.com>
2023-12-14 14:55:16 +0000

    file update

---
+++ Frontend/src/Routes.js
@@ -0,0 +1,35 @@
+import App from './App'
+import { createBrowserRouter , createRoutesFromElements, Route , RouterProvider } from 'react-router-dom';
+//components
+import Notfound from './components/Notfound';
+//pages
+import Piece from './Piece';
+import BaseLayout from './BaseLayout';
+import {loaderFunction} from './Api'
+import {loaderFunctionPiece} from './Piece'
+
+const router = createBrowserRouter(
+  createRoutesFromElements(
+    <Route  path='/' element={<BaseLayout/>}>
+      <Route 
+      index 
+      loader={loaderFunction}
+      element={<App/>}/>
+      
+      <Route 
+      path='/:id' 
+      element={<Piece/>}
+      loader={loaderFunctionPiece}
+      />
+
+    </Route>
+  )
+)
+function Routes() {
+  
+return (
+      <RouterProvider router={router}/> );
+}
+
+
+export default Routes;
