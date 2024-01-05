0feb5693e6e4a0c1684ddc91d6cb84079cdd8c50
Osele Chidera <oselechideraemmanuel123@gmail.com>
2024-01-05 15:33:11 +0000

    modified spelling of Content-Type

--- Frontend/src/Api.js
+++ Frontend/src/Api.js
@@ -2,7 +2,7 @@
 export const loaderFunction = async ()=>{
    //using the port from the server
    const fetchUrl = '/data'
-   const res = await fetch(fetchUrl ,  {method: "GET", headers: {"Content-type": "application/json;charset=UTF-8"}})
+   const res = await fetch(fetchUrl ,  {method: "GET", headers: {"Content-Type": "application/json;charset=UTF-8"}})
    const data  = await res.json()
    return data;
 }
