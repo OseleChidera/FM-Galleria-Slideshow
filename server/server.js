547a18c76f9542ac27d2bcbc9fc5b2797f28dea4
Osele Chidera <oselechideraemmanuel123@gmail.com>
2024-01-05 15:15:16 +0000

    server config

--- server/server.js
+++ server/server.js
@@ -6,9 +6,6 @@ const path = require('path');
 
 app.use(express.static(path.join(__dirname, '/public')));
 
-app.get("/", function (req, res) {
-    res.sendFile(path.join(__dirname,'public','index.html' ))
-})
 
 app.get('/data', (req, res) => {
     fs.readFile('./data.json', 'utf8', (err, data) => {
