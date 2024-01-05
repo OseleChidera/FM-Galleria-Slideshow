a357295dfdd5727b37d64a3782662a9104232bab
Osele Chidera <oselechideraemmanuel123@gmail.com>
2024-01-05 00:04:42 +0000

    updated file path

--- server/server.js
+++ server/server.js
@@ -5,38 +5,41 @@ app.use(cors());
 const port = 4000
 const fs = require('fs');
 
+const path = require('path');
+
 app.get('/data', (req, res) => {
     res.header('Access-Control-Allow-Origin', '*');
-    fs.readFile('./data.json', 'utf8', (err, data) => {
+    const filePath = path.join(process.cwd(), 'data.json');
+
+    fs.readFile(filePath, 'utf8', (err, data) => {
         if (err) {
             console.error(err);
-            return;
+            return res.status(500).send('Error reading JSON file');
         }
         try {
             const jsonData = JSON.parse(data);
             res.json(jsonData);
         } catch (error) {
             console.error('Error parsing JSON:', error);
+            res.status(500).send('Error parsing JSON data');
         }
     });
 });
 
-
 app.get('/data/:id', (req, res) => {
     const id = req.params.id;
+    const filePath = path.join(process.cwd(), 'data.json');
 
-    fs.readFile('./data.json', 'utf8', (err, data) => {
+    fs.readFile(filePath, 'utf8', (err, data) => {
         if (err) {
             console.error('Error reading JSON file:', err);
-            res.status(500).send('Error reading JSON file');
-            return;
+            return res.status(500).send('Error reading JSON file');
         }
         try {
             const jsonData = JSON.parse(data);
             const item = jsonData.find(obj => obj.id === Number(id));
             if (!item) {
-                res.status(404).send('Item not found');
-                return;
+                return res.status(404).send('Item not found');
             }
             res.json(item);
         } catch (err) {
